bd3cafe8a697dda8b3436e0513fe15b30eba4a1c
charles22224 <26nielschaj@washk12.org>
2025-02-27 11:29:51 -0700

    pick a card

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const prompt = require('prompt-sync')();
 let Cardsuit = prompt("Name a Card Suit?(Hearts, Diamonds, Clubs, Spades) ");
-let number = prompt("Now Name a Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10 Jack, Queen, King? ");
+let number = prompt("Now Name a Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10 Jack, Queen or King? ");
 let greeting = "You picked " + number + " of " + Cardsuit + "! Have Fun!";
 console.log(greeting);
-
