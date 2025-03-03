3b49ae55e22c67c357d1c1886a6e4708b94fb12d
charles22224 <26nielschaj@washk12.org>
2025-03-03 10:49:18 -0700

    pick a fight

--- madlibs.js
+++ madlibs.js
@@ -1,5 +1,6 @@
 const prompt = require("prompt-sync")(); // Ask for user inputs
 let gameCharacter = prompt("Enter a game character: ");
+let place = prompt("Enter a Place: ");
 let weapon = prompt("Enter a weapon: ");
-let story = "One time " + gameCharacter + " was on a mission to go fight Beru at Jeju island."
+let story = "One time " + gameCharacter + " was on a mission to go fight Beru at " + place + " with a " + weapon +
 console.log(story);
